e682e6e3324ce637e4d96bab34e2c56185200425
Ben Nocera <bnocera17@gmail.com>
2023-04-01 19:26:27 -0400

    add first passing test

--- test/test.js
+++ test/test.js
@@ -5,14 +5,14 @@ const path = require('path')
 const { exec } = require('child_process')
 const { stdout } = require('process')
 
-describe('', function () {
-  it('', function (done) {
-    exec('node prepend-image-resolution.js ', (error, stdout, stderr) => {
+describe('prepend-image-resolution tests', function () {
+  it('app has been supplied with nothing', function (done) {
+    exec('node prepend-image-resolution.js', (error, stdout, stderr) => {
       if (stderr) {
         console.log(`stderr: ${stderr}`)
         return
       }
-      expect(stdout).to.include('')
+      expect(stdout).to.include('please supply valid image type')
       done()
     })
   })
