de6596ee72754c0b0bd95ce52b5c92836850e2f2
Ben Nocera <bnocera17@gmail.com>
2023-04-05 23:16:47 -0400

    image dimension test passing

--- test/test.js
+++ test/test.js
@@ -28,14 +28,18 @@ describe('prepend-image-resolution tests', function () {
     })
   })
 
-  /*it('app should check if dimensions were added to file name', function (done) {
+  it('app should check if dimensions were added to file name', function (done) {
     exec('node prepend-image-resolution.js images', (error, stdout, stderr) => {
       if (stderr) {
         console.log(`stderr: ${stderr}`)
         return
       }
-      expect(stdout).to.include('')
+      const images = fs.readdirSync('images')
+      console.log(images)
+      expect(images).to.include('347x200_Pumpkins.jpeg')
+      expect(images).to.include('360x360_Donut2.jpeg')
+      expect(images).to.include('455x360_Donut1.jpeg')
       done()
     })
-  })*/
+  })
 })
